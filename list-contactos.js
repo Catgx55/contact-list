980da3cf3e8df417844b9c654bcdc3398f9ecab2
Cesar Trujillo <catgx55@gmail.com>
2023-07-18 21:52:12 -0500

    Editando funcion eliminar contacto y agregar contacto

--- list-contactos.js
+++ list-contactos.js
@@ -1,21 +1,31 @@
 function agregar(agregarContacto){
+    console.log("Agregando contatos: ");
     contactos.push(agregarContacto);
-
+    //console.log("__________________________________ ");
 }
 
-function eliminar(eliminarContacto){
-    contactos.pop(eliminarContacto);
-}
 
-function elimanar2(eliminarContacto1){
-    contactos.shift(eliminarContacto1);
-    return eliminarContacto1
+function eliminar(agregarContacto){
+    console.log("Eliminado Contactos: ");
+    let index = contactos.indexOf(agregarContacto);
+    if(index !== -1){
+        contactos.splice(index,1);
+    }
+    //console.log("__________________________________ ");
 }
 
 function listaContactos(){
+    console.log("Lista contactos: ");
     for(let i = 0; i < contactos.length; i++){
-        console.log(contactos[i]);
+        console.log(i+1,". ", contactos[i]);
     }
+    console.log("__________________________________ ");
 }
 
 const contactos = ["Cesar Trujillo", "Andy Trujillo", "Henry Trujillo", "Henry Giraldo",];
+
+listaContactos()
+agregar("Maria Torres");
+listaContactos()
+eliminar("Henry Trujillo");
+listaContactos();
